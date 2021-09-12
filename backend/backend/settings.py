08d2f94cedad7379023ca084dea574a756baa15f
Alberto Castañeda <albertcastaned@gmail.com>
2021-09-11 22:13:40 -0500

    setup static files django for production/staging

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-35!h^w0^vffmbcmg^r4+popgut
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['api.staging.gaqsa.com', 'api.prod.gaqsa.com', 'localhost']
 
 
 # Application definition
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
