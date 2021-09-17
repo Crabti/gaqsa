0cd22c5d341133fe99fea9950bb4184bac413031
Alberto Castañeda <albertcastaned@gmail.com>
2021-09-17 10:44:29 -0500

    fix: change python test coverage commands in CI

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
     'rest_framework',
     'backend',
     'users',
@@ -187,3 +188,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
