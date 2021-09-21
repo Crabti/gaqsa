9bbaf11d4e2c03b2407845d6b01ec5b7dfa4d8e7
Topiltzin Hernández Mares <topiltzin.hernandez@wizeline.com>
2021-09-21 13:38:17 -0500

    feat: add basic home view

---
+++ frontend/src/Routes/index.ts
@@ -0,0 +1,21 @@
+import { RouteProps } from 'react-router-dom';
+import HomeView from 'views/Home';
+
+export interface RoutesType {
+  path: string;
+  view: RouteProps['component'];
+  props?: Omit<RouteProps, 'path' | 'component'>;
+}
+
+export interface Routes {
+  [key: string]: RoutesType;
+}
+
+const routes: Routes = {
+  home: {
+    path: '',
+    view: HomeView,
+  },
+};
+
+export default routes;
