379f65d052c2ad992fc8e72c8f2bbf5053247c05
Alberto Castañeda <albertcastaned@gmail.com>
2021-09-21 11:24:12 -0500

    hotfix: staging and production environment vars retrieval

--- backend/backend/staging.wsgi.py
+++ backend/backend/staging.wsgi.py
@@ -1,3 +1,4 @@
+
 """
 WSGI config for backend project.
 
@@ -8,8 +9,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
-os.environ['ENV'] = 'staging'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-application = get_wsgi_application()
+
+
+def application(environ, start_response):
+    os.environ['ENV'] = 'staging'
+    os.environ['STAGING_DB_NAME'] = environ['STAGING_DB_NAME']
+    os.environ['STAGING_DB_USER'] = environ['STAGING_DB_USER']
+    os.environ['STAGING_DB_PASSWORD'] = environ['STAGING_DB_PASSWORD']
+    os.environ['SECRET_KEY'] = environ['SECRET_KEY']
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+    return get_wsgi_application()(environ, start_response)
