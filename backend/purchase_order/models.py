3efd21ad37ff071f96dc52da822098163ae7329f
Carlos Sánchez <carlossanchez@MacBook-Pro-de-Carlos.local>
2021-09-22 00:10:55 -0500

    Subir avance modelo orden de compra

--- backend/purchase_order/models.py
+++ backend/purchase_order/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Purchase_Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    key = models.CharField(max_length=8, verbose_name="Clave", unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
