da44b4a48fad7c5cabb74470a215d5cca9abfd95
Carlos Sánchez <carlossanchez@MacBook-Pro-de-Carlos.local>
2021-09-22 21:30:53 -0500

    Añadir modelo

--- backend/purchase_order/models.py
+++ backend/purchase_order/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 from users.models import User
+from products.models import Product
+from providers.models import Provider
 
 
 class Purchase_Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    key = models.CharField(max_length=8, verbose_name="Clave", unique=True)
+    key = models.CharField(max_length=8, verbose_name="OrdenCompra",
+                           unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Requisition(models.Model):
+    key = models.CharField(max_length=8, verbose_name="Requisicion",
+                           unique=True)
+    purchase_order = models.ForeignKey(Purchase_Order,
+                                       on_delete=models.CASCADE)
+    idProveedor = models.ForeignKey(Provider, on_delete=models.CASCADE)
+    idProduct = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity_req = models.IntegerField()
