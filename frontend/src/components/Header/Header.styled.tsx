054cc4a724766d22eebe6c5b747ef93523a66ba0
Topiltzin Hernández Mares <topiltzin.hernandez@wizeline.com>
2021-09-26 16:42:32 -0500

    chore: add logo to header

--- frontend/src/components/Header/Header.styled.tsx
+++ frontend/src/components/Header/Header.styled.tsx
@@ -1,8 +1,10 @@
 import { Layout, Menu as MenuAntd } from 'antd';
 import styled, { css } from 'styled-components';
+import LogoImage from 'static/img/logo.png';
 
 const headerCommonStyles = css`
   background: ${({ theme }) => theme.colors.primary};
+  position: relative;
 `;
 
 export const HeaderCont = styled(Layout.Header)`
@@ -18,4 +20,11 @@ export const HeaderCont = styled(Layout.Header)`
 export const Menu = styled(MenuAntd)`
   display: flex;
   align-items: flex-end;
+`;
+
+export const Logo = styled.div`
+  background-image: url(${LogoImage});
+  background-repeat: no-repeat;
+  background-size: contain;
+  transform: scale(1.2); // this is just to adjust the image size
 `;
