c1a897c8b262bb57a0a965d4dac31be91bd181ad
Carlos Sánchez <carlossanchez@MacBook-Pro-de-Carlos.local>
2021-09-28 18:20:35 -0500

    Add context shopping cart
    Signed-off-by: Carlos Sánchez <carlossanchez@MacBook-Pro-de-Carlos.local>

---
+++ frontend/src/hooks/shoppingCart/index.tsx
@@ -0,0 +1,63 @@
+import { Product } from '@types';
+import React, { useState } from 'react';
+
+export interface ShoppingCartProductType {
+  product: Product;
+  amount: number;
+}
+
+export interface ShoppingCartType {
+    products: ShoppingCartProductType[];
+    total: number;
+    addProducts: (newProduct: ShoppingCartProductType) => void;
+}
+
+export const ShoppingCartContext = (
+  React.createContext<ShoppingCartType>(undefined!)
+);
+
+export const ShoppingCartContextProvider: React.FC = ({ children }) => {
+  const [products, setProducts] = useState<ShoppingCartProductType[]>([]);
+  const [total, setTotal] = useState<ShoppingCartType['total']>(0);
+
+  const persistProducts = (newProducts: ShoppingCartProductType[]): void => {
+    setProducts(newProducts);
+
+    let newTotal = 0;
+
+    newProducts.forEach((e) => {
+      newTotal += e.amount * e.product.price;
+    });
+
+    setTotal(newTotal);
+
+    localStorage.setItem('shoppingCart', JSON.stringify({
+      products: newProducts,
+      total: newTotal,
+    }));
+  };
+
+  const addProducts = (newProduct: ShoppingCartProductType): void => {
+    if (products.some((e) => e.product.id === newProduct.product.id)) {
+      persistProducts(
+        products.map((e) => (e.product.id === newProduct.product.id
+          ? { ...e, amount: e.amount + newProduct.amount }
+          : e)),
+      );
+      return;
+    }
+    persistProducts([...products, newProduct]);
+  };
+
+  return (
+    <ShoppingCartContext.Provider value={{ products, total, addProducts }}>
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+};
+
+const useShoppingCart = (): ShoppingCartType => React.useContext(
+  ShoppingCartContext,
+);
+
+export default useShoppingCart;
