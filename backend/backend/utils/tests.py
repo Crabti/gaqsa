c7103f7ef714c591bf129235923c1af26ca296aa
Alberto Castañeda <albertcastaned@gmail.com>
2021-10-04 18:12:07 -0500

    refactor: improve basetest case speed by 90%

--- backend/backend/utils/tests.py
+++ backend/backend/utils/tests.py
@@ -1,80 +1,44 @@
 from users.factories.user import UserFactory
 from django.test import TestCase
-from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import Group
-from django.urls import reverse
-from http import HTTPStatus
 from rest_framework.test import APIClient
-from django.conf import settings
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class BaseTestCase(TestCase):
 
     @classmethod
     def setUpTestData(cls) -> None:
-        # Faster password hasher
-        settings.PASSWORD_HASHERS = [
-            'django.contrib.auth.hashers.MD5PasswordHasher'
-        ]
 
-        # Create provider user
-        cls.provider_password = 'provider'
-        cls.provider_user = UserFactory.create(
-            password=make_password(cls.provider_password)
-        )
+        cls.provider_user = UserFactory.create()
         provider_group = Group.objects.get(name='Proveedor')
         cls.provider_user.groups.add(provider_group)
-        cls.client_password = 'client'
-        cls.client_user = UserFactory.create(
-            password=make_password(cls.client_password)
-        )
+
+        cls.client_user = UserFactory.create()
         client_group = Group.objects.get(name='Cliente')
         cls.client_user.groups.add(client_group)
-        cls.admin_password = 'admin'
-        cls.admin_user = UserFactory.create(
-            password=make_password(cls.admin_password)
-        )
+
+        cls.admin_user = UserFactory.create()
         admin_group = Group.objects.get(name='Administrador')
         cls.admin_user.groups.add(admin_group)
-        return super().setUpTestData()
+        cls.anonymous = APIClient()
 
-    def setUp(self) -> None:
-        # Anonymous client
-        self.anonymous = APIClient()
-
-        self.provider_client = APIClient()
-        response = self.provider_client.post(
-            reverse('login'),
-            {
-                'username': self.provider_user.username,
-                'password': self.provider_password
-            }
-        )
-        self.provider_client.credentials(
-            HTTP_AUTHORIZATION="Bearer " + response.data['access']
+        cls.provider_client = APIClient()
+        access = RefreshToken.for_user(cls.provider_user).access_token
+        cls.provider_client.credentials(
+            HTTP_AUTHORIZATION="Bearer " + str(access)
         )
 
         # Not named client so we dont override the already existing self.client
-        self.service_client = APIClient()
-        response = self.service_client.post(
-            reverse('login'),
-            {
-                'username': self.client_user.username,
-                'password': self.client_password,
-            }
-        )
-        self.service_client.credentials(
-            HTTP_AUTHORIZATION="Bearer " + response.data['access'],
+        cls.service_client = APIClient()
+        access = RefreshToken.for_user(cls.client_user).access_token
+        cls.service_client.credentials(
+            HTTP_AUTHORIZATION="Bearer " + str(access)
         )
 
-        self.admin_client = APIClient()
-        response = self.admin_client.post(
-            reverse('login'),
-            {
-                'username': self.admin_user.username,
-                'password': self.admin_password
-            }
+        cls.admin_client = APIClient()
+        access = RefreshToken.for_user(cls.admin_user).access_token
+        cls.admin_client.credentials(
+            HTTP_AUTHORIZATION="Bearer " + str(access)
         )
-        self.admin_client.credentials(
-            HTTP_AUTHORIZATION="Bearer " + response.data['access']
-        )
+        return super().setUpTestData()
