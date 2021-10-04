c0e799347a7b2c0fb0cc6f98f0ed666733796ea2
Alberto Castañeda <albertcastaned@gmail.com>
2021-10-04 17:53:29 -0500

    feat: add authentication requirement to all endpoints and refactor tests

--- backend/backend/utils/tests.py
+++ backend/backend/utils/tests.py
@@ -5,66 +5,76 @@ from django.contrib.auth.models import Group
 from django.urls import reverse
 from http import HTTPStatus
 from rest_framework.test import APIClient
+from django.conf import settings
 
 
 class BaseTestCase(TestCase):
-    def setUp(self) -> None:
-        # Create provider user and client for convenient use
-        provider_password = 'provider'
-        self.provider_user = UserFactory.create(
-            password=make_password(provider_password)
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        # Faster password hasher
+        settings.PASSWORD_HASHERS = [
+            'django.contrib.auth.hashers.MD5PasswordHasher'
+        ]
+
+        # Create provider user
+        cls.provider_password = 'provider'
+        cls.provider_user = UserFactory.create(
+            password=make_password(cls.provider_password)
         )
         provider_group = Group.objects.get(name='Proveedor')
-        self.provider_user.groups.add(provider_group)
+        cls.provider_user.groups.add(provider_group)
+        cls.client_password = 'client'
+        cls.client_user = UserFactory.create(
+            password=make_password(cls.client_password)
+        )
+        client_group = Group.objects.get(name='Cliente')
+        cls.client_user.groups.add(client_group)
+        cls.admin_password = 'admin'
+        cls.admin_user = UserFactory.create(
+            password=make_password(cls.admin_password)
+        )
+        admin_group = Group.objects.get(name='Administrador')
+        cls.admin_user.groups.add(admin_group)
+        return super().setUpTestData()
+
+    def setUp(self) -> None:
+        # Anonymous client
+        self.anonymous = APIClient()
+
         self.provider_client = APIClient()
         response = self.provider_client.post(
             reverse('login'),
             {
                 'username': self.provider_user.username,
-                'password': provider_password
+                'password': self.provider_password
             }
         )
-        self.assertTrue(response.status_code, HTTPStatus.OK)
         self.provider_client.credentials(
             HTTP_AUTHORIZATION="Bearer " + response.data['access']
         )
 
-        # Create provider user and client for convenient use
-        client_password = 'client'
-        self.client_user = UserFactory.create(
-            password=make_password(client_password)
-        )
-        client_group = Group.objects.get(name='Cliente')
-        self.client_user.groups.add(client_group)
         # Not named client so we dont override the already existing self.client
         self.service_client = APIClient()
         response = self.service_client.post(
             reverse('login'),
             {
                 'username': self.client_user.username,
-                'password': client_password,
+                'password': self.client_password,
             }
         )
-        self.assertTrue(response.status_code, HTTPStatus.OK)
         self.service_client.credentials(
             HTTP_AUTHORIZATION="Bearer " + response.data['access'],
         )
-        # Create admin user and client for convenient use
-        admin_password = 'admin'
-        self.admin_user = UserFactory.create(
-            password=make_password(admin_password)
-        )
-        admin_group = Group.objects.get(name='Administrador')
-        self.admin_user.groups.add(admin_group)
+
         self.admin_client = APIClient()
         response = self.admin_client.post(
             reverse('login'),
             {
                 'username': self.admin_user.username,
-                'password': admin_password
+                'password': self.admin_password
             }
         )
         self.admin_client.credentials(
             HTTP_AUTHORIZATION="Bearer " + response.data['access']
         )
-        self.assertTrue(response.status_code, HTTPStatus.OK)
