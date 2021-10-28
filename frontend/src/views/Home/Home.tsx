e30d812b1c77369e21c9d1dd1765479598900081
Alberto Castañeda <albertcastaned@gmail.com>
2021-10-28 17:34:28 -0500

    fix: remove home title and mock windows.matchMedia

--- frontend/src/views/Home/Home.tsx
+++ frontend/src/views/Home/Home.tsx
@@ -1,25 +1,9 @@
 import React from 'react';
 import Title from 'components/Title';
-import { Row, Typography } from 'antd';
-import useAuth from 'hooks/useAuth';
 
-const Home: React.ViewComponent = ({ verboseName }) => {
-  const { user } = useAuth();
-  return (
-    <>
-      <Title viewName={verboseName} parentName="Gaqsa" />
-      <Row justify="center">
-        <Typography.Title level={4}>
-          Bienvenido
-          {
-        (user && user?.first_name && user.last_name)
-          ? ` ${user.first_name} ${user.last_name}` : null
-        }
-        </Typography.Title>
-      </Row>
+const Home: React.ViewComponent = ({ verboseName }) => (
+  <Title viewName={verboseName} parentName="Gaqsa" />
 
-    </>
-  );
-};
+);
 
 export default Home;
