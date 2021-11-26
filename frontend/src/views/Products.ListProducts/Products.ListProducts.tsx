27268ce42b03f46b55fb027d21632b1605dcf8b9
Carlos Sánchez <carlossanchez@MacBook-Pro-de-Carlos.local>
2021-11-26 11:24:50 -0600

    Add again order view

--- frontend/src/views/Products.ListProducts/Products.ListProducts.tsx
+++ frontend/src/views/Products.ListProducts/Products.ListProducts.tsx
@@ -54,7 +54,7 @@ const ListProducts: React.VC = ({ verboseName, parentName }) => {
   ] = useState<ProductGroup[] | undefined>(undefined);
   const [filtered, setFiltered] = useState<ProductGroup[]>([]);
   const {
-    clear, productsSh,
+    productsSh,
   } = useShoppingCart();
   const resetFiltered = useCallback(
     () => setFiltered(products || []), [products],
@@ -108,38 +108,11 @@ const ListProducts: React.VC = ({ verboseName, parentName }) => {
     fetchProducts();
   };
 
-  const onOrderFailed = () : void => {
-    notification.error({
-      message: '¡Ocurrió un error al intentar generar la orden de compra!',
-      description: 'Intentalo después.',
-    });
-  };
-
   const createOrder = async () : Promise<void> => {
     setLoading(true);
     if (productsSh.length > 0) {
-      const [, error] = await backend.orders.createOne({
-        productsSh,
-      });
-      if (error) {
-        onOrderFailed();
-        history.replace('/productos/');
-      } else {
-        notification.success({
-          message: '¡Petición de orden creado exitosamente!',
-          description: 'Su orden de compra ha sido recibida y será procesada.'
-            + 'El proveedor le informará lo mas pronto posible '
-            + 'el estatus de su solicitud.',
-        });
-        history.replace('/pedidos/cliente');
-      }
-    } else {
-      notification.error({
-        message: '¡Carrito de compras vacío!',
-        description: 'Inserte alguna cantidad en algún producto que requiera',
-      });
+      history.replace('/pedidos');
     }
-    clear();
     setLoading(false);
   };
 
