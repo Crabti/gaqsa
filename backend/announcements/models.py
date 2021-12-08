577f468e13922a6bee11ec33cc106890e414a5bb
Topiltzin Hernández Mares <topiltzin.hernandez@wizeline.com>
2021-12-08 17:58:04 -0600

    chore: Add Announcement model and migration

--- backend/announcements/models.py
+++ backend/announcements/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from users.models import Profile
+
+
+class Announcement(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=140, verbose_name="Título")
+    content = models.TextField(verbose_name="Contenido", blank=True)
+    created_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    file_url = models.URLField(verbose_name="URL del archivo")
