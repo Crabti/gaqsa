1d3562a820b646ea77b36d097775bb0bb245603a
Alberto Castañeda <albertcastaned@gmail.com>
2022-01-17 19:22:59 -0700

    feat: add initial home welcome message

--- frontend/src/views/Home/Home.tsx
+++ frontend/src/views/Home/Home.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import Title from 'components/Title';
+import useAuth from 'hooks/useAuth';
+import { Typography } from 'antd';
+import { Content } from 'antd/lib/layout/layout';
 
-const Home: React.ViewComponent = ({ verboseName }) => (
-  <Title viewName={verboseName} />
+const Home: React.ViewComponent = ({ verboseName }) => {
+  const { user } = useAuth();
+  const welcomeMessage = () : string => {
+    if (user) {
+      if (user.first_name && user.last_name) {
+        return `Bienvenido, ${user.first_name} ${user.last_name}`;
+      }
+      if (user.first_name) {
+        return `Bienvenido, ${user.first_name}`;
+      }
+    }
+    return 'Bienvenido';
+  };
 
-);
-
+  return (
+    <Content>
+      <Title viewName={verboseName} />
+      <Typography.Title level={3} style={{ marginLeft: '1em' }}>
+        { welcomeMessage() }
+      </Typography.Title>
+    </Content>
+  );
+};
 export default Home;
